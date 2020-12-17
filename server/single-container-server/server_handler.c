963139319ca8ac4d17c1690af9e5f0f5a4100dc2
thisUnkn0wn <freddylukai@gmail.com>
2020-12-17 18:04:40 -0500

    made minor changes to server

--- server/single-container-server/server_handler.c
+++ server/single-container-server/server_handler.c
@@ -20,7 +20,7 @@
 struct CALL_RET {
     int code;
     char* content;
-} call_ret;
+};
 
 cJSON* handle_request_1(cJSON* request);
 
@@ -32,7 +32,9 @@ cJSON* handle_request_4(cJSON* request);
 
 cJSON* handle_request_5(cJSON* request);
 
-call_ret* getcert_call(char* username, char* password, char* csr_str);
+struct CALL_RET* isuservalid_call(char* username, char* password);
+
+struct CALL_RET* getcert_call(char* csr_str);
 
 cJSON* get_response_obj(cJSON* request) {
     int request_type;
@@ -69,8 +71,8 @@ cJSON* get_response_obj(cJSON* request) {
 cJSON* handle_request_1(cJSON* request) {
     // todo: tighten up these bounds as reasonable
     char username[256] = {0}, hpass[1024] = {0}, csr_str[16384] = {0};
-    cJSON* response_obj, content_obj;
-    call_ret* call_return;
+    cJSON* response_obj, *content_obj;
+    struct CALL_RET* call_return;
 
     // first check that the request has all the required components
     if (cJSON_IsString(cJSON_GetObjectItemCaseSensitive(request, "username")) == false) {
@@ -90,49 +92,53 @@ cJSON* handle_request_1(cJSON* request) {
     strncpy(hpass, cJSON_GetObjectItemCaseSensitive(request, "password")->valuestring, 1024);
     strncpy(csr_str, cJSON_GetObjectItemCaseSensitive(request, "public_key")->valuestring, 16384);
 
-    // make the call
-    call_return = getcert_call(username, hpass, csr_str);
-
-    // create a JSON response based on the call
+    // setup JSON return
     response_obj = cJSON_CreateObject();
     cJSON_AddNumberToObject(response_obj, "response_type", 1);
     content_obj = cJSON_CreateObject();
     cJSON_AddItemToObject(response_obj, "content", content_obj);
     cJSON_AddNumberToObject(content_obj, "response_type", 1);
-    switch (call_return->code) {
-        case 0:
-            cJSON_AddNumberToObject(response_obj, "status_code", 200);
-            cJSON_AddStringToObject(content_obj, "certificate", cJSON_CreateString(call_return->content));
-            break;
-        case 1:
-            cJSON_AddNumberToObject(response_obj, "status_code", 401);
-            cJSON_AddStringToObject(content_obj, "error_msg", "Invalid username or password.\n");
-            break;
-        case 2:
-            cJSON_AddNumberToObject(response_obj, "status_code", 401);
-            cJSON_AddStringToObject(content_obj, "error_msg", "Invalid key presented for signature.\n");
-            break;
-        default:
-            fprintf(stderr, "Invalid return.\n");
-            cJSON_Delete(response_obj);
-            free(call_return->content);
-            return NULL;
+
+    // make calls
+    call_return = isuservalid_call(username, password);
+    if (call_return->code != 0) {
+        cJSON_AddNumberToObject(response_obj, "status_code", 401);
+        cJSON_AddStringToObject(content_obj, "error_msg", "Invalid username or password.\n");
+        free(call_return);
+        return response_obj;
     }
+    free(call_return);
+    call_return = getcert_call(csr_str);
+    if (call_return->code != 0) {
+        cJSON_AddNumberToObject(response_obj, "status_code", 401);
+        cJSON_AddStringToObject(content_obj, "error_msg", "Invalid key provided.\n");
+        free(call_return);
+        return response_obj;
+    }
+    cJSON_AddNumberToObject(response_obj, "status_code", 200);
+    cJSON_AddItemToObject(response_obj, "certificate", cJSON_CreateString(call_return->content));
     free(call_return->content);
+    free(call_return);
     return response_obj;
 }
 
+// calls isuservalid and gets a response
+struct CALL_RET* isuservalid_call(char* username, char* password) {
+    //todo Jason: fill this in
+    // return 0 if valid, else anything else
+    // do not allocate call_ret->content
+    // you should allocate for call_ret though; the caller will free
+}
+
 // calls getcert and gets a response
-call_ret* getcert_call(char* username, char* password, char* csr_str) {
+struct CALL_RET* getcert_call(char* csr_str) {
     //todo Jason: fill this in; assume csr_str is the full string of the public key/csr
     // you probably want to save the csr to a .csr, get the intermediate cert, and sign the .csr
-    // after checking if username is valid
     // call_ret is a structure designed to make it simple if we eventually go to TCP
-    // the first field is code: for this, 0 if successful, 1 if username/password was not authorized, 2 if csr is bad
-    // the other field is content: whatever string content we might need to return
-    // so check if user is valid
-    // write csr to a file if needed, get intermediate and sign
-    // if it succeeds, malloc call_ret->content, read certificate to that
+    // the first field is code: for this, 0 if successful, otherwise anything else
+    // if 0 is returned, I expect content in the call_ret, otherwise it should be free
+    // the content should be the full text of the certificate
+    // so you can write the csr, sign it, write the output cert, and then read it to a string and stick it in content
 }
 
 //changepwd request
